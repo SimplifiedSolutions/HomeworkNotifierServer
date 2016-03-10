da8a52879476a6128d94b7be8436bf1ce39346e9
gopher8 <nicholas.victoryofthepeople@gmail.com>
2016-03-09 18:59:18 -0700

    Small router function for AddUser call.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('basic.html', { root:  'public' });
+});
+router.post('/AddUser', function(req, res, next) {
+  //var jsonObject = req.body; //json object
+  //var jsonObject = JSON.parse(req.body); //string
+  console.log(req.body);
+  var name = req.body.name;
+  var netId = req.body.netId;
+  var password = req.body.password;
+
+  // Mongodb calls
+
+  var jsonresult = { success: true, message: "Your awesome beans.", data: { key: "a1key" } };
+  console.log(jsonresult);
+  res.status(200).json(jsonresult);
 });
 
 module.exports = router;
